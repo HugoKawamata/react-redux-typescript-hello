c3f3fc317168c5173085914bccf1ecab516ea353
Hugo Kawamata <hugokawamata@gmail.com>
2017-09-21 20:58:04 +1000

    Create repo

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import "./App.css";
+import { Provider } from "react-redux";
+import HelloContainer from "./containers/HelloContainer";
+
+import store from "./store";
+
+const logo = require("./logo.svg");
+
+class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React</h2>
+          </div>
+          <p className="App-intro">
+            <HelloContainer />
+          </p>
+        </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
